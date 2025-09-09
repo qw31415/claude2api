b33ec8d1c4ac22c84a31cfb181624b62e15eb035
EIN001 <124855012+qw31415@users.noreply.github.com>
2025-09-09 09:49:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -18,24 +18,4 @@ func main() {
 	r.Run(config.ConfigInstance.Address)
 }
 
-// main.go
-package main
-
-import (
-  "os"
-  "github.com/gin-gonic/gin"
-)
-
-func main() {
-  r := gin.Default()
-  // 读取 Render 自动注入的端口
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"  // 本地开发时的兜底值
-  }
-  r.GET("/", func(c *gin.Context) {
-    c.String(200, "hello render")
-  })
-  r.Run(":" + port)
-}
 
